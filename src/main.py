e4da132112598e15e275cf0a5e40de141667239b
lolo-chan <rohanmchetan@gmail.com>
2026-05-30 04:51:28 +0000

    Initialize repo and build textnode class and tests

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import TextNode, TextType
+
+def main():
+    new_node = TextNode("dummy text", TextType.TEXT)
+    print(new_node)
+
+main()
---
+++ src/test_textnode.py
@@ -0,0 +1,21 @@
+import unittest
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is correct", TextType.ITALIC)
+        repr = f"TextNode(This is correct, italic, None)"
+        self.assertEqual(node.__repr__(), repr)
+
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is another text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
