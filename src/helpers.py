3b17969311be77289a1aa8c461340d2392c41ca0
lolo-chan <rohanmchetan@gmail.com>
2026-06-04 10:54:00 +0000

    Clean helpers

--- src/helpers.py
+++ src/helpers.py
@@ -110,14 +110,3 @@ def markdown_to_blocks(markdown):
             result.append(clean_block)
     return result
 
-markdown = """
-            This is **bolded** paragraph
-
-            This is another paragraph with _italic_ text and `code` here
-            This is the same paragraph on a new line
-
-            - This is a list
-            - with items
-            """
-print(markdown_to_blocks(markdown))
-
